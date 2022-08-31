dab10954ef6ad920c886ba15b3b88a33b1ed5979
유현명 <aahspringaa4@naver.com>
2022-08-31 11:08:32 +0900

    ♻️ :: SerializedName @field
    clear compile

--- data-auth/src/main/java/com/example/data_auth/remote/request/SignInRequest.kt
+++ data-auth/src/main/java/com/example/data_auth/remote/request/SignInRequest.kt
@@ -3,6 +3,6 @@ package com.example.data_auth.remote.request
 import com.google.gson.annotations.SerializedName
 
 data class SignInRequest(
-    @SerializedName("id") val id: String,
-    @SerializedName("password") val password: String
+    @field:SerializedName("id") val id: String,
+    @field:SerializedName("password") val password: String
 )
