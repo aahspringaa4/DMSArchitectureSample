8e8b647f61c2bd65e456a6a6e6da310d2452898a
유현명 <aahspringaa4@naver.com>
2022-08-31 11:09:14 +0900

    ⚡️ :: SignIn Repository Implementation

--- data-auth/src/main/java/com/example/data_auth/repository/UserRepositoryImpl.kt
+++ data-auth/src/main/java/com/example/data_auth/repository/UserRepositoryImpl.kt
@@ -1,5 +1,33 @@
 package com.example.data_auth.repository
 
-class UserRepositoryImpl {
+import com.example.data_auth.remote.datasource.implementation.RemoteUserDataSource
+import com.example.data_auth.remote.request.SignInRequest
+import com.example.database.datasource.LocalUserDataSource
+import com.example.database.param.LocalUserViewParam
+import com.example.domain_auth.param.LoginParam
+import com.example.domain_auth.repository.UserRepository
+import javax.inject.Inject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
+class UserRepositoryImpl @Inject constructor(
+    private val remoteUserDataSource: RemoteUserDataSource,
+    private val localUserDataSource: LocalUserDataSource
+): UserRepository{
+
+
+    override suspend fun login(
+        loginParam: LoginParam
+    ) {
+        val response = remoteUserDataSource.postUserSignIn(SignInRequest(
+            id = loginParam.id,
+            password = loginParam.password
+        ))
+
+        localUserDataSource.fetchViewBoolean(LocalUserViewParam(
+            meal = response.meal,
+            point = response.point,
+            notice = response.notice
+        ))
+    }
 }
